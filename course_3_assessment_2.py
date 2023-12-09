a122b9db1c30c0856be46b53adca8670130b9eb9
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-12-10 01:39:06 +0300

    Update course_3_assessment_2.py

--- course_3_assessment_2.py
+++ course_3_assessment_2.py
@@ -6,7 +6,7 @@ print(map_testing)
 # Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that begin with B.
 
 countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
-b_countries=list(filter (lambda value: value.startswith("B"),countries))
+b_countries=list(filter(lambda value: value.startswith("B"),countries))
 print(b_countries)
 
 # Below, we have provided a list of tuples that contain the names of Game of Thrones characters. Using list comprehension, create a list of strings called first_names that contains only the first names of everyone in the original list.
