7d82d7adc5b3f62bcd02d5f893b4ebdcf2b55043
prof "F.M.P <107954137+Fatimamehmud@users.noreply.github.com>
2023-12-10 00:20:19 +0300

    Update course_3_assessment_2.py

--- course_3_assessment_2.py
+++ course_3_assessment_2.py
@@ -2,3 +2,9 @@
 lst_check = ['plums', 'watermelon', 'kiwi', 'strawberries', 'blueberries', 'peaches', 'apples', 'mangos', 'papaya']
 map_testing=list(map((lambda value :"Fruit: "+value),lst_check))
 print(map_testing)
+
+# Below, we have provided a list of strings called countries. Use filter to produce a list called b_countries that only contains the strings from countries that begin with B.
+
+countries = ['Canada', 'Mexico', 'Brazil', 'Chile', 'Denmark', 'Botswana', 'Spain', 'Britain', 'Portugal', 'Russia', 'Thailand', 'Bangladesh', 'Nigeria', 'Argentina', 'Belarus', 'Laos', 'Australia', 'Panama', 'Egypt', 'Morocco', 'Switzerland', 'Belgium']
+b_countries=list(filter (lambda value: value.startswith("B"),countries))
+print(b_countries)
